e144684109c2a04a6bbee9d0e6225bbe3ed9283e
Robert Turner <hello@henryave.com>
2021-10-17 00:44:28 -0400

    Refactor and style

--- app-stack-problem.js
+++ app-stack-problem.js
@@ -19,6 +19,7 @@ class Player {
     this.attackDmg = attackDmg;
   }
 }
+
 class Enemy {
   constructor(name, health, attackDmg) {
     this.name = name;
@@ -34,56 +35,47 @@ const enemy = new Enemy();
 function initializePlayer() {
   // Create player with 10 health and random attack damage between 1 and 3
   player.name = playerNameInput.value;
-  playerNameSpan.innerText = player.name;
   player.health = 10;
-  userHealthSpan.innerText = player.health;
-  player.attackDmg = Math.floor(Math.random() * 3) + 1; // Math.floor(Math.random() * (max - min + 1)) + min;
-  userDmgSpan.innerText = player.attackDmg;
+  player.attackDmg = Math.floor(Math.random() * 3);
+  playerNameSpan.innerText = player.name;
 }
 
 function initializeEnemy () {
   // Create enemy with 10 health and random attack damage between 1 and 3
   enemy.name = enemyNames[Math.floor(Math.random() * enemyNames.length)];
-  enemyNameSpan.innerText = enemy.name;
   enemy.health = 10;
-  enemyHealthSpan.innerText = enemy.health;
-  enemy.attackDmg = Math.floor(Math.random() * 3) + 1; // Math.floor(Math.random() * (max - min + 1)) + min;
-  enemyDmgSpan.innerText = enemy.attackDmg;
+  enemy.attackDmg = Math.floor(Math.random() * 3);
+  enemyNameSpan.innerText = enemy.name;
 }
 
 function startGame() {
-  if (playerNameInput.value) {
-    initializePlayer();
-    initializeEnemy();
+  initializePlayer();
+  initializeEnemy();
+  updateUI();
+}
+
+function updateUI() {
+  userHealthSpan.innerText = player.health;
+  userDmgSpan.innerText = player.attackDmg;
+  enemyHealthSpan.innerText = enemy.health;
+  enemyDmgSpan.innerText = enemy.attackDmg;
+}
+
+function deadOrAlive() {
+  if (enemy.health <= 0) {
+    alert("You win!");
+  } else if (player.health <= 0) {
+    alert("You dead!");
   }
 }
 
 function playerAttack() {
-  // Only allow attack if game is not over
-  if (player.health && enemy.health) {
-    // Player attacks
-    // If enemy health is zero or greater after attack set it, otherwise set 0
-    if (enemy.health - player.attackDmg >= 0) {
-      enemy.health -= player.attackDmg;
-    } else {
-      enemy.health = 0
-    }
-    enemyHealthSpan.innerText = enemy.health;
-    // Enemy returns attack
-    // If enemy health is zero or greater after attack set it, otherwise set 0
-    if (player.health - enemy.attackDmg >= 0) {
-      player.health -= enemy.attackDmg;
-    } else {
-      player.health = 0
-    }
-    userHealthSpan.innerText = player.health;
-
-    if (enemy.health <= 0) {
-      alert("You win!");
-    } else if (player.health <= 0) {
-      alert("You dead!");
-    }
-  }
+  // Player attacks
+  enemy.health = enemy.health - player.attackDmg;
+  // Enemy returns attack
+  player.health = player.health - enemy.attackDmg;
+  updateUI();
+  deadOrAlive();
 }
 
 startGameBtn.addEventListener("click", startGame);
